37c888474faf9ebffe06311b9765fd086c0e43de
oliver.drobnik <oliver.drobnik@799d14a0-bb11-11de-8dfb-43c0dffa5306>
2009-10-17 14:20:21 +0000

    initial import

---
+++ trunk/WSDLdocument.h
@@ -0,0 +1,27 @@
+//
+//  WSDLdocument.h
+//  SOAP
+//
+//  Created by Oliver on 14.10.09.
+//  Copyright 2009 Drobnik.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "XMLdocument.h"
+
+@interface WSDLdocument : XMLdocument <XMLdocumentDelegate>
+{
+	XMLelement *service;
+	NSArray *ports;
+	
+	XMLelement *types;
+	XMLelement *schema;
+}
+
+- (NSArray *)portNames;
+
+
+- (void)writeClassFilesForPort:(NSString *)portName;
+
+@end
---
+++ trunk/XMLdocument.h
@@ -0,0 +1,69 @@
+//
+//  XMLdocument.h
+//
+//  Created by Oliver on 02.09.09.
+//  Copyright 2009 Drobnik.com. All rights reserved.
+//
+
+@class XMLdocument;
+
+#import "XMLelement.h"
+#import <Foundation/NSXMLParser.h>
+
+@protocol XMLdocumentDelegate <NSObject>
+
+@optional
+
+- (void) xmlDocumentDidFinish:(XMLdocument *)xmlDocuments;
+- (void) xmlDocument:(XMLdocument *)xmlDocument didFailWithError:(NSError *)error;
+
+- (NSURLCredential *) userCredentialForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge;
+@end
+
+
+
+@interface XMLdocument : NSObject
+
+#ifdef TARGET_OS_MAC
+<NSXMLParserDelegate>
+#endif
+
+{
+	XMLelement *documentRoot;
+	
+	id <XMLdocumentDelegate> delegate;
+
+	// parsing 
+	XMLelement *currentElement;
+	
+	// lists
+	NSMutableDictionary *namespaces;
+	
+	// url loading
+	NSMutableData *receivedData;
+	NSURLConnection *theConnection;
+	
+	BOOL doneLoading;
+}
+
+@property (nonatomic, retain) XMLelement *documentRoot;
+@property (nonatomic, assign) id <XMLdocumentDelegate> delegate;
+@property (nonatomic, readonly) BOOL doneLoading;
+
++ (XMLdocument *) documentWithString:(NSString *)xmlString;
++ (XMLdocument *) documentWithData:(NSData *)data;
++ (XMLdocument *) documentWithContentsOfFile:(NSString *)path;
++ (XMLdocument *) documentWithContentsOfFile:(NSString *)path delegate:(id<XMLdocumentDelegate>)XMLdocumentDelegate;
++ (XMLdocument *) documentWithContentsOfURL:(NSURL *)url;
++ (XMLdocument *) documentWithContentsOfURL:(NSURL *)url delegate:(id<XMLdocumentDelegate>)XMLdocumentDelegate;
+
+- (id) initWithString:(NSString *)xmlString;
+- (id) initWithData:(NSData *)data;
+- (id) initWithContentsOfFile:(NSString *)path;
+- (id) initWithContentsOfFile:(NSString *)path delegate:(id<XMLdocumentDelegate>)XMLdocumentDelegate;
+- (id) initWithContentsOfURL:(NSURL *)url;
+- (id) initWithContentsOfURL:(NSURL *)url delegate:(id<XMLdocumentDelegate>)XMLdocumentDelegate;
+
+- (void) cancelLoading;
+
+@end
